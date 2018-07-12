0478b22d69fbb24f8a7c831d8af61b4f8abeb7c7
= <litalch@3base.co.il>
2018-07-13 02:04:29 +0300

    add integration tests

---
+++ test/integration-test.js
@@ -0,0 +1,199 @@
+'use strict';
+
+const app = require('../app'),
+  chai = require('chai'),
+  request = require('supertest');
+
+const expect = chai.expect;
+
+describe('Calculator API Integration Tests', function() {
+  describe('#POST / calculate', function() { 
+    
+
+	it('should get status 200 ok', function(done) { 
+      request(app).post('/calculate')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+        
+  	  });
+    
+	it('should get header content-type: json', function(done) { 
+      request(app).post('/calculate')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+        
+  	  });
+
+    it('should get an empty json object', function(done) { 
+      request(app).post('/calculate')
+        .end(function(err, res) { 
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.deep.equal({}); 
+          expect(res.body).to.be.empty; 
+          //task = res.body; 
+          done(); 
+        }); 
+  	  });
+  	
+	let state; 
+  	it('should get json {display:1} on sending stateObject null and input 1', function(done) { 
+		request(app)
+  		.post('/calculate')
+  		.send({
+			    'input': '1'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"1"}); 
+          state = res.body;
+
+          done(); 
+        }); 
+  	  });
+  	
+  	it('should get json {display:12} on sending previous stateObject and input 2', function(done) { 
+
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '2'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"12"}); 
+          state = res.body;
+          done(); 
+        }); 
+  	  });
+
+  	  it('should get json {display:12} on sending previous stateObject and input +', function(done) { 
+  		
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '+'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"12"}); 
+          state = res.body;
+          done(); 
+        }); 
+  	  });
+
+ 	it('should get json {display:3} on sending previous stateObject and input 3', function(done) { 
+  		
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '3'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"3"}); 
+          state = res.body;
+          done(); 
+        }); 
+  	  });
+
+ 	it('should get json {display:35} on sending previous stateObject and input 5', function(done) { 
+  		
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '5'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"35"}); 
+          state = res.body;
+          done(); 
+        }); 
+  	  });
+	
+	it('should get json {display:47} on sending previous stateObject and input =', function(done) { 
+  		
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '='
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"47"}); 
+          state = res.body;
+          done(); 
+        }); 
+  	  });
+
+    it('should get json {display:6} on sending previous stateObject and input 6', function(done) { 
+  		
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '6'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"6"}); 
+          state = res.body;
+          done(); 
+        }); 
+  	  });
+  
+  	it('should get json {display:62} on sending previous stateObject and input 2', function(done) { 
+  		
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '2'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"62"}); 
+          state = res.body;
+          done(); 
+        }); 
+  	  });
+
+  	it('should get json {display:62} on sending previous stateObject and input /', function(done) { 
+  		
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '/'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.deep.include({'display':"62"}); 
+          state = res.body;
+          done(); 
+        }); 
+  	  });
+	
+  	it('should get ERR on sending previous stateObject and input 0', function(done) { 
+  		
+		request(app)
+  		.post('/calculate')
+  		.send({
+  				'calculatorState': state,
+			    'input': '/'
+  			})
+  		.end(function(err, res) { 
+          expect(res.statusCode).to.equal(200); 
+          expect(res.body).to.equal("ERR"); 
+          done(); 
+        }); 
+  	  });
+
+	});
+
+});
--- test/test-calculator-state.js
+++ test/test-calculator-state.js
@@ -27,6 +27,12 @@ describe('calculateNextState', function() {
 		expect(calculator.calculateNextState(state1,input1)).to.include(expectedState1);
 	})
 
+	
+	it('should return valid json and correct display when recieved input as additional number and the json state includes all the fields ', () => {
+		expect(calculator.calculateNextState({"display": "1" , "part1": "1", "part2": "" , "action": ""},2)).to.include({display: "12" , part1: "12"});
+	})
+
+
 	let state2 = {"display": "12" , "part1": "12"} ;
 	let input2 = '+';
 	let expectedState2 = {display: "12" , part1: "12" , "action":"+"};
@@ -45,14 +51,24 @@ describe('calculateNextState', function() {
 		.to.include({display: "15" , part1: "15" ,part2: "", "action":""});
 	})
 
+	it('should return valid json ,equality_done should be true, on receiving input =', () => {
+		expect(calculator.calculateNextState({display: "3" , part1: "12" ,part2:"3", "action":"+"},"="))
+		.to.include({display: "15" , part1: "15" ,part2: "", "action":"" , "equality_done":true});
+	})
+
 	it('should return valid json , display should be the prev calculation,part2 should be empty and action should be -, on receiving input - after =', () => {
 		expect(calculator.calculateNextState({display: "15" , part1: "15" ,part2:"", "action":""},"-"))
 		.to.include({display: "15" , part1: "15" ,part2: "", "action":"-"});
 	})
 
 	it('should return valid json , display should be the current input ,part2,action should be empty , on receiving input number after =', () => {
-		expect(calculator.calculateNextState({display: "15" , part1: "15" ,part2:"", "action":""},"7"))
+		expect(calculator.calculateNextState({display: "15" , part1: "15" ,part2:"", "action":"" , "equality_done":true},"7"))
 		.to.include({display: "7" , part1: "7" ,part2: "", "action":""});
+	})
+
+	it('should return valid json , display should be the current input ,part2,action should be empty , on receiving input number after input number', () => {
+		expect(calculator.calculateNextState({display: "7" , part1: "7" ,part2: "", "action":""},"1"))
+		.to.include({display: "71" , part1: "71" ,part2: "", "action":""});
 	})
 
 	it('should return ERR on receiving input + at the begining', () => {
